1af149fbc9c20dc7de2d11fedde8465ffbdbbab9
@igor_gagarkin <i.gagarkin@gmail.com>
2025-09-01 20:56:29 +0300

    add homework 3.5

---
+++ src/components/ProductList.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable max-len -->
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+    <ul class="catalog__list">
+          <!-- // eslint-disable-next-line vue/require-v-for-key, vue/require-v-for-key -->
+          <ProductItem v-for='(product, index) in products' :key="index" :product="product"/>
+        </ul>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-unresolved
+import ProductItem from './ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props: ['products'],
+};
+
+</script>
