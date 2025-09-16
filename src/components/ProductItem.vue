fd978d515406209ea56570fd6c72c2b6f3c9535f
@igor_gagarkin <i.gagarkin@gmail.com>
2025-09-16 20:16:29 +0300

    add homework 4.4

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -2,45 +2,45 @@
 <!-- eslint-disable max-len -->
 <!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-<li class="catalog__item">
-<a class="catalog__pic" href="#">
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
 
-              <img :src="product.image" :alt="product.title">
-            </a>
+      <img :src="product.image" :alt="product.title">
+    </a>
 
-            <h3 class="catalog__title">
-              <a href="#">
-                {{product.title}}
-              </a>
-            </h3>
+    <h3 class="catalog__title">
+      <a href="#">
+        {{ product.title }}
+      </a>
+    </h3>
 
-            <span class="catalog__price">
-              {{product.price}}
-            </span>
+    <span class="catalog__price">
+      {{ product.price }}
+    </span>
 
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
+    <ul class="colors colors--black">
+      <li class="colors__item">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color">
+          <span class="colors__value" style="background-color: #73B6EA;">
+          </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color">
+          <span class="colors__value" style="background-color: #8BE000;">
+          </span>
+        </label>
+      </li>
+      <li class="colors__item">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color">
+          <span class="colors__value" style="background-color: #222;">
+          </span>
+        </label>
+      </li>
+    </ul>
   </li>
 
 </template>
@@ -50,6 +50,11 @@
 export default {
   name: 'ProductItem',
   props: ['product'],
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
 };
 
 </script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,10 @@
 <!-- eslint-disable max-len -->
 <!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-    <ul class="catalog__list">
-          <!-- // eslint-disable-next-line vue/require-v-for-key, vue/require-v-for-key -->
-          <ProductItem v-for='(product, index) in products' :key="index" :product="product"/>
-        </ul>
+  <ul class="catalog__list">
+    <!-- // eslint-disable-next-line vue/require-v-for-key, vue/require-v-for-key -->
+    <ProductItem v-for='product in products' :key="product.id" :product="product" />
+  </ul>
 </template>
 
 <script>
