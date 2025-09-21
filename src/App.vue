0fdb5d567988a15f99cbbd7c2d63da7332679296
@igor_gagarkin <i.gagarkin@gmail.com>
2025-09-21 14:28:15 +0300

    add lesson 4.5

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
+<!-- eslint-disable max-len -->
 <!-- eslint-disable vue/require-v-for-key -->
 <!-- eslint-disable no-multiple-empty-lines -->
 <!-- eslint-disable vuejs-accessibility/label-has-for -->
 <!-- eslint-disable max-len -->
 <template>
-  <section class="catalog">
-    <ProductList :products="products"></ProductList>
-    <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"></BasePagination>
-  </section>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductFilter :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId" />
+      <section class="catalog">
+        <ProductList :products="products"></ProductList>
+        <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage"></BasePagination>
+      </section>
+
+    </div>
+  </main>
+
 </template>
 
 <script>
@@ -14,23 +32,47 @@
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
       page: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        // eslint-disable-next-line max-len
+        filteredProducts = filteredProducts.filter((product) => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        // eslint-disable-next-line max-len
+        filteredProducts = filteredProducts.filter((product) => product.price < this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId) {
+        // eslint-disable-next-line max-len
+        filteredProducts = filteredProducts.filter((product) => product.categoryId === this.filterCategoryId);
+      }
+
+      return filteredProducts;
+    },
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products.length;
+      return this.filteredProducts.length;
     },
   },
 };
